f74d86ac89d57b543d61d3c05447adb11959759f
roneil <roneilsembrano@gmail.com>
2020-02-25 00:38:33 -0800

    got the segue working

---
+++ Flixitup/MovieDetailsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MovieDetailsViewController.swift
+//  Flixitup
+//
+//  Created by Roneil Sembrano on 2/24/20.
+//  Copyright © 2020 Roneil Sembrano. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    var movie: [String: Any]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let cell = sender as! UITableViewCell
+//        let indexPath = tableView.indexPath(for: cell)!
+//        let movie = movies[indexPath.row]
+//    }
+    
+
+}
